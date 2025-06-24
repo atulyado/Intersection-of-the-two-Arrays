8a39a7ef1bdda043295ef45e1d702a4a55e287ce
atulyado <133553524+atulyado@users.noreply.github.com>
2025-06-24 19:03:48 +0700

    Update IntersectionArrays.java

--- IntersectionArrays.java
+++ IntersectionArrays.java
@@ -1,18 +1,18 @@
 import java.util.HashSet;
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> set = new HashSet<Integer>();
+        HashSet<Integer> set = new HashSet<Integer>();//set is use for unqiue number 
         for(int num : nums1){
             set.add(num);
         }
 
-        HashSet<Integer> result = new HashSet<Integer>();
+        HashSet<Integer> result = new HashSet<Integer>();//here we check if num2 have num1 and store it in result for unique elements
         for(int num : nums2){
             if(set.contains(num)){
                 result.add(num);
             }
         }
-        int[] finalResult = new int[result.size()];
+        int[] finalResult = new int[result.size()];//this one covert the result set to an array
             int i =0;
             for(int num : result){
                 finalResult[i++] = num;
